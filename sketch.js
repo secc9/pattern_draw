9b2d848a6761c5af2128238edb2e442eb8dbedce
secc9 <78687901+secc9@users.noreply.github.com>
2023-01-17 20:31:46 +0000

    added index, p5js, p5 sound, sketchjs and css files

---
+++ sketch.js
@@ -0,0 +1,55 @@
+let xPos;
+let yPos;
+let amt;
+
+
+
+function setup() {
+frameRate(6);
+    createCanvas(displayWidth, displayHeight);
+    // background(0);
+
+    noLoop();
+
+}
+
+function draw() {
+background(255,1);
+    let x = random(100, 1000);
+    let y = random(100, 1000);
+    let alpha = frameCount % random(100, 200);
+ ///////////////////////////////////////////// 
+ // code stolen/liberated from Joe's random walk
+    let nXpos = xPos + random(-amt,amt);
+    let nYpos = yPos + random(amt,amt);
+////////////////////////////////////////////////
+
+
+    noFill();
+   noStroke();
+    // strokeWeight(random(0.5,2));
+    // stroke(0, alpha);
+    fill(random(100, 255), random(255), 0, alpha);
+ // bezier(x1,y1, z1, x2, y2, z2, x3, y3, z3, x4, y4, z4)
+    bezier(mouseX, mouseY, xPos, yPos, xPos+nXpos, yPos+nYpos, pmouseX, pmouseY,);
+
+    xPos = nXpos;
+    yPos = nYpos;
+
+    console.log();
+}
+
+function mousePressed(){
+
+
+    xPos = mouseX;
+    yPos = mouseY;
+    amt = random(-3, 3);
+    loop();
+
+}
+
+function keyPressed(){
+
+    save("image###.png")
+}
